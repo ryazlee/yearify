74268e575002db8bb69716c9e483ecfe7a3938c5
Ryan Lee <ryan.j.lee99@gmail.com>
2024-12-03 15:45:42 -0800

    Fix styling a lil bit

--- src/components/calendar/ColorLegend.tsx
+++ src/components/calendar/ColorLegend.tsx
@@ -1,24 +1,35 @@
 import { Box, Typography } from "@mui/material";
 import { CATEGORY_COLORS } from "../types";
 
+const COLORBOX_STYLE = {
+    width: '25px',
+    height: '25px',
+    borderRadius: '4px',
+};
+
+const TEXT_STYLE = {
+    fontSize: '15px',
+    fontWeight: 'bold',
+};
+
 export const ColorLegend = () => {
     return (
-        <Box display={'flex'} flexDirection={'row'} gap={"10px"} padding={"10px 0"} >
+        <Box display={'flex'} flexDirection={'row'} gap={"10px"} padding={"10px 0"}>
             <Box display={'flex'} flexDirection={'row'} gap={"10px"} >
-                <Box width={'20px'} height={'20px'} bgcolor={CATEGORY_COLORS.travel} />
-                <Typography>Travel</Typography>
+                <Box style={COLORBOX_STYLE} bgcolor={CATEGORY_COLORS.travel} />
+                <Typography style={TEXT_STYLE}>Travel</Typography>
             </Box>
             <Box display={'flex'} flexDirection={'row'} gap={"10px"} >
-                <Box width={'20px'} height={'20px'} bgcolor={CATEGORY_COLORS.social} />
-                <Typography>Social</Typography>
+                <Box style={COLORBOX_STYLE} bgcolor={CATEGORY_COLORS.social} />
+                <Typography style={TEXT_STYLE}>Social</Typography>
             </Box>
             <Box display={'flex'} flexDirection={'row'} gap={"10px"} >
-                <Box width={'20px'} height={'20px'} bgcolor={CATEGORY_COLORS.fitness} />
-                <Typography>Fitness</Typography>
+                <Box style={COLORBOX_STYLE} bgcolor={CATEGORY_COLORS.fitness} />
+                <Typography style={TEXT_STYLE}>Fitness</Typography>
             </Box>
             <Box display={'flex'} flexDirection={'row'} gap={"10px"} >
-                <Box width={'20px'} height={'20px'} bgcolor={CATEGORY_COLORS.personal} />
-                <Typography>Personal</Typography>
+                <Box style={COLORBOX_STYLE} bgcolor={CATEGORY_COLORS.personal} />
+                <Typography style={TEXT_STYLE}>Personal</Typography>
             </Box>
         </Box >
     );
